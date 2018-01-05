e79f6f14ab8b127b1178c65a07b034ddcad744d1
Tlangelani Mboweni <tlangelanimb@miway.co.za>
2018-01-05 09:06:54 +0200

    add vue components

---
+++ src/app/App.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>MiWay Insurance Ltd.</p>
+</template>
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
