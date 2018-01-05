370b68c997f490df440ee9345735de6e63f83a37
Tlangelani Mboweni <tlangelanimb@miway.co.za>
2018-01-05 13:54:57 +0200

    uglify js and minify css

--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -1,6 +1,8 @@
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.config')
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = merge(baseConfig, {
     module: {
@@ -13,5 +15,9 @@ module.exports = merge(baseConfig, {
                 })
             }
         ]
-    }
+    },
+    plugins: [
+        new UglifyJsPlugin(),
+        new OptimizeCssAssetsPlugin()
+    ]
 });
